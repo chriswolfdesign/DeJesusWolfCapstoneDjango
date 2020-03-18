dbd4300d2d9612394b0f46f1554c7caeb1d13507
Chris <chriswolfdesign@gmail.com>
2020-03-18 19:54:14 -0400

    Adding node_modules and venv :_(

---
+++ venv/pythonenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chriswolf/Documents/My_Code/Coursework/WesternCarolinaUniversity/Capstone/django_server/venv/pythonenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
