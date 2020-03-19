173513a879b00807fd5cfb7b48e2b602a312dbff
Chris <chriswolfdesign@gmail.com>
2020-03-19 08:16:35 -0400

    Attempted to change wsgi file

--- django_server/wsgi.py
+++ django_server/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_server.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_server.settings')
+#
+# application = get_wsgi_application()
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
