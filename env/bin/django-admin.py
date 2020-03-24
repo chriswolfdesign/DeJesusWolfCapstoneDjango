7538e7dbb24d691674210c62db14c7ee3ad3f22d
Chris <chriswolfdesign@gmail.com>
2020-03-24 10:06:28 -0400

    Moved to env for consistency

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chriswolf/Documents/My_Code/Coursework/WesternCarolinaUniversity/Capstone/django_server/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
