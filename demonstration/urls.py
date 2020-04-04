9b9a56a378e8ec2fd81890a45ca41bdd0b961691
Ellery <>
2020-04-04 05:36:58 -0400

    Added the Login and Register page

--- demonstration/urls.py
+++ demonstration/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.entry, name="entry"),
+    path('home', views.index, name="home"),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
 ]
--- demonstration/views.py
+++ demonstration/views.py
@@ -1,9 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
-from django.http import HttpResponse
+from .forms import CreateUserForm
+
+
+def entry(request):
+    return redirect('login')
 
 
 def index(request):
     return render(request, 'demonstration/index.html')
+
+
+def loginPage(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR Password is incorrect.')
+            return render(request, 'accounts/login.html', context)
+    return render(request, 'demonstration/login.html', context)
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for ' + user)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'demonstration/register.html', context)
