10c2aab9831c4b4f29afdb7e14496c25e0fe9954
Ellery <>
2020-04-06 15:25:01 -0400

    Switch Project and Static Root

--- django_server/settings.py
+++ django_server/settings.py
@@ -130,19 +130,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 # Chris
-
+'''
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+'''
 
 
 # Ellery
-'''
+
 PROJECT_ROOT = os.path.join(os.path.dirname(os.path.dirname(
     os.path.join(os.path.abspath(__file__)))), 'demonstration')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-'''
 
 
 # Extra lookup directories for collectstatic to find static files
