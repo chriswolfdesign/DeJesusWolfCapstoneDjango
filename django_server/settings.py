c49df72dd315fd7c51fcddb0f8902e512e9d1251
Ellery <>
2020-04-06 13:14:40 -0400

    Uncommented STATICFILES_DIRS

--- django_server/settings.py
+++ django_server/settings.py
@@ -141,12 +141,13 @@ PROJECT_ROOT = os.path.join(os.path.dirname(os.path.dirname(
     os.path.join(os.path.abspath(__file__)))), 'demonstration')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+'''
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-'''
+
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
