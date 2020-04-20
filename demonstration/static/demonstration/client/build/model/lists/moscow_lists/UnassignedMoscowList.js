6a7f67b3e277ed31a3a345ae3317870f40e7c2f5
Chris <chriswolfdesign@gmail.com>
2020-04-20 13:45:24 -0400

    Pusing to fix GitHub acting up

---
+++ demonstration/static/demonstration/client/build/model/lists/moscow_lists/UnassignedMoscowList.js
@@ -0,0 +1,29 @@
+"use strict";
+/**
+ * UnassignedMoscowList.ts
+ *
+ * A class that will generate a list for MoSCoW cards that have not yet been assigned.
+ *
+ * @author Ellery De Jesus
+ * @author Chris Wolf
+ * @version 2.0.0 (April 15, 2020)
+ */
+exports.__esModule = true;
+var List_1 = require("../List");
+var MoscowStatus_1 = require("../../enums/MoscowStatus");
+var BacklogStatus_1 = require("../../enums/BacklogStatus");
+var UnassignedMoscowList = /** @class */ (function () {
+    function UnassignedMoscowList() {
+    }
+    // Constructor deliberately left out
+    /**
+     * generates an Unassigned List for MoSCoW board
+     *
+     * @return {List} a Must Have List
+     */
+    UnassignedMoscowList.prototype.generateList = function () {
+        return new List_1.List('Unassigned', MoscowStatus_1.MoscowStatus.UNASSIGNED, BacklogStatus_1.BacklogStatus.NONE);
+    }; // end generateList
+    return UnassignedMoscowList;
+}()); // end MustList
+exports.UnassignedMoscowList = UnassignedMoscowList;
